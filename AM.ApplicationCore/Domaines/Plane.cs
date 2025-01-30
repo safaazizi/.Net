d9cec1947262eb00c3c6f9eb65d56ae5455adda9
JdMary <mariem.jrad@esprit.tn>
2025-01-30 22:31:54 +0100

    console app project + library class project + plane class

---
+++ AM.ApplicationCore/Domaines/Plane.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.ApplicationCore.Domaine
+{
+    //method 1 : creation of en enum
+    //method 2 : create an enum under directory domain
+    public enum PlaneType
+    {
+        Boeing,Airbus
+    }
+    public class Plane
+    {
+        public int Capacity { get; set; }
+        public DateTime ManufactureDate { get; set; }
+        public int PlaneId {  get; set; }
+        public PlaneType PlaneType { get; set; }
+    }
+}
